dd9f9c3ca74d565978b3b0366dc60b7c998fcc68
Mark Saiget <saiget@gmail.com>
2017-11-18 14:32:45 -0800

    Add Ean.jsx component.

---
+++ app/components/Ean.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Ean extends React.Component {
+    constructor(props) {
+        super(props);
+    } 
+
+
+    render() {
+        return (
+            <h3>Ean Calculator Test</h3>
+            
+        )
+    }
+}
+
+export default Ean;
