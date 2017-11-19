b2283e709387b4065034187874051aa72ce6de44
Mark Saiget <saiget@gmail.com>
2017-11-19 02:38:13 -0800

    Add Button Component.

---
+++ app/components/Button.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+require('./Button.css');
+
+class Button extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.props.onChange(this.props.id);
+    }
+    render() {
+        let className = !this.props.disabled ? 'button' : 'button active';
+        return (
+            <button
+                className={className}
+                type='submit'
+                disabled={this.props.disabled}
+                onClick={this.handleChange}
+            >
+                {this.props.text}
+            </button>
+        )
+    }
+}
+
+export default Button;
