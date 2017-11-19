0313c2d7c6400c6fa93e924fad0e8a9199dd97d8
Mark Saiget <saiget@gmail.com>
2017-11-18 19:20:04 -0800

    Add step to NumberInput.

--- app/components/Ean.jsx
+++ app/components/Ean.jsx
@@ -10,8 +10,9 @@ class Ean extends React.Component {
             totalVolume: 200,
             initialPressure: 0,
             finalPressure: 3500,
-            initialO2: 32,
-            finalO2: 32
+            initialO2: 32.0,
+            finalO2: 32.0,
+            fillO2: 20.9
         };
 
         this.onChange = this.onChange.bind(this);
@@ -30,7 +31,7 @@ class Ean extends React.Component {
         let Ci = Number(this.state.initialO2/100);
         let Cf = Number(this.state.finalO2/100);
 
-        let fillO2Conc = 0.21;
+        let fillO2Conc = Number(this.state.fillO2/100);
         
         let psiO2 = (Ci*Pf + fillO2Conc*Pi - fillO2Conc*Pf - Ci*Pi)/(1-fillO2Conc);
         return _.round(Pi+psiO2, 0);
@@ -40,7 +41,7 @@ class Ean extends React.Component {
         let O2Add = this.getO2Pressure();
         return (
             <div>
-                <h3>Ean Calculator Test</h3>
+                <h3>Ean Calculator</h3>
                 <div>
                 <NumberInput
                     onChange={this.onChange}
@@ -55,6 +56,7 @@ class Ean extends React.Component {
                     units='psi'
                     id='initialPressure'
                     value={this.state.initialPressure}
+                    step='100'
                 />
                 <NumberInput
                     onChange={this.onChange}
@@ -64,6 +66,7 @@ class Ean extends React.Component {
                     value={this.state.initialO2}
                     min='0'
                     max='100'
+                    step='.1'
                 />
                 <NumberInput
                     onChange={this.onChange}
@@ -71,6 +74,7 @@ class Ean extends React.Component {
                     units='psi'
                     id='finalPressure'
                     value={this.state.finalPressure}
+                    step='100'
                 />
                 <NumberInput
                     onChange={this.onChange}
@@ -80,13 +84,28 @@ class Ean extends React.Component {
                     value={this.state.finalO2}
                     min='0'
                     max='100'
+                    step='.1'
+                />
+                <NumberInput
+                    onChange={this.onChange}
+                    label='source gas O2 concentration'
+                    units='%'
+                    id='fillO2'
+                    value={this.state.fillO2}
+                    min='0'
+                    max='100'
+                    step='.1'
                 />
                 </div>
-                {O2Add && 
+                {O2Add ? 
                     <div>
                         Add O2 to {O2Add} <br/>
-                        Add air to {this.state.finalPressure} <br/>
-                    </div>}
+                        Add {this.state.fillO2}% to {this.state.finalPressure} <br/>
+                    </div> :
+                    <div>
+                        Add {this.state.fillO2}% to {this.state.finalPressure} <br/>
+                    </div>
+                    }
             </div>
         )
     }
